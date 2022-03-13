4cefe0b12d29d86693ac97fd37d1e3be9d0e9d2e
anaemerubio <anaemerubio@gmail.com>
2022-03-13 22:31:37 +0100

    Exercice 2

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
 // DOM button
 const $JOKE_BUTTON = document.querySelector("#jokeButton");
+const $PRINT_JOKE = document.querySelector("#printJoke");
 
 // url API jokes
 const API_URL = "https://icanhazdadjoke.com";
@@ -7,18 +8,17 @@ const API_URL = "https://icanhazdadjoke.com";
 // Que cuando hagan click se llame a la API
 $JOKE_BUTTON.addEventListener("click", () => getJoke(API_URL));
 
-// async await 
 async function getJoke(API_URL) {
     let joke = await fetch("https://icanhazdadjoke.com/", {
         headers: {
           Accept: "application/json"
         }
-      });
+    });
 
     let jokeJson = await joke.json();
 
+    // Enseñar en consola
     console.log(jokeJson.joke);
+    // Enseñar en html
+    $PRINT_JOKE.innerHTML = jokeJson.joke;
 }
-
-// Exercice 1
-

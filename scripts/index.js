b9a8376c2fe300f41eb4f96ed4e9deab5b4fe956
anaemerubio <anaemerubio@gmail.com>
2022-03-15 10:54:51 +0100

    Exercice 5

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,9 @@
 // URL API bromas
-const API_URL = "https://icanhazdadjoke.com";
+const API_DAD_JOKES = "https://icanhazdadjoke.com";
 // API meteo
-const API_METEO = 'https://api.openweathermap.org/data/2.5/weather?q=barcelona,es&lang=en&units=metric&appid=b3da016d9d44d0dbc6cae5783b7e3f8a';
+const API_METEO = "https://api.openweathermap.org/data/2.5/weather?q=barcelona,es&lang=en&units=metric&appid=b3da016d9d44d0dbc6cae5783b7e3f8a";
+// API Chuck Norris
+const API_CHUCK_NORRIS = "https://api.chucknorris.io/jokes/random";
 // link p HTML para mostrar por pantalla
 const $SHOW_METEO = document.querySelector("#showMeteo");
 // Pulsar boton
@@ -11,15 +13,18 @@ const $PRINT_JOKE = document.querySelector("#printJoke");
 // Array puntuacion usuario
 let reportJoke = [];
 
-// Cuando hagan click se llame a la API dadjokes
-$JOKE_BUTTON.addEventListener("click", () => getJoke(API_URL));
+// Cuando hagan click muestra la broma de la API (Ahora random)
+/* $JOKE_BUTTON.addEventListener("click", () => getJoke(API_DAD_JOKES)); */
+/* $JOKE_BUTTON.addEventListener("click", () => getChuckNorrisJoke(API_CHUCK_NORRIS)); */
+$JOKE_BUTTON.addEventListener("click", () => showRandomJoke());
+
 
 // Cuando se cargue la página llama a la API meteorologica
 window.addEventListener("load", () => {getMeteo(API_METEO); });
 
 // Llamada de API dadjokes (Ej 1 y 2) https://stackoverflow.com/questions/36975619/how-to-call-a-rest-web-service-api-from-javascript
-async function getJoke(API_URL) {
-  let joke = await fetch(API_URL, {
+async function getJoke(API_DAD_JOKES) {
+  let joke = await fetch(API_DAD_JOKES, {
       headers: {
         Accept: "application/json"
       }
@@ -43,7 +48,7 @@ function scoreJoke(score) {
   console.log(reportJoke);
 }
 
-// Llamar API meteo (Ej4) https://openweathermap.org/current#geo
+// Llamar API meteo (Ej4) https://openweathermap.org/current#geo | https://www.youtube.com/watch?v=GXrDEA3SIOQ&ab_channel=ShanjaiRaj
 async function getMeteo() {
   const meteo = await fetch(API_METEO, {
     method: "GET",
@@ -59,4 +64,23 @@ async function getMeteo() {
 
   console.log(weather);
   $SHOW_METEO.innerHTML = weather;
+}
+
+// Llamar API Chuck Norris (Ej 5 Parte 1)
+async function getChuckNorrisJoke(API_CHUCK_NORRIS) {
+  let jokeChuck = await fetch(API_CHUCK_NORRIS);
+  let jokeChuckJSON = await jokeChuck.json();
+
+  console.log(jokeChuckJSON.value);
+  $PRINT_JOKE.innerHTML = `${jokeChuckJSON.value}`;
+}
+
+// Randomizador de broma (Ej 5 Parte 2)
+function showRandomJoke() {
+  let counter = Math.floor(Math.random()*(2 + 1));
+  if (counter == 0) {
+      getJoke(API_DAD_JOKES);
+  } else {
+      getChuckNorrisJoke(API_CHUCK_NORRIS);
+  }
 }
